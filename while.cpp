a66104d220d679ea158eadb4897976009fa7c7c5
Nikita4501 <butakovnikita000@mail.ru>
2024-09-19 09:40:04 +0300

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int int1 =15;
+    int int2 = 20;
+    cout << "int1=" << int1 << "  " << "int2=" << int2 << endl;
+    while (int1 != int2) {
+        int1++;
+
+    }
+    cout << "int1=" << int1 << "  " << "int2=" << int2 << endl;
+
+    int int3 = 22;
+    int int4 = 12;
+    cout << "int3=" << int3 << "  " << "int4=" << int4 << endl;
+    while (int3 > int4) {
+        int4++;
+
+    }
+    cout << "int3=" << int3 << "  " << "int4=" << int4 << endl;
+
+    int1 = 15;
+    int2 = 20;
+    cout << "int1=" << int1 << "  " << "int2=" << int2 << endl;
+    while (int1 < int2) {
+        int1++;
+
+    }
+    cout << "int1=" << int1 << "  " << "int2=" << int2 << endl;
+
+    int3 = 22;
+    int4 = 12;
+    cout << "int3=" << int3 << "  " << "int4=" << int4 << endl;
+    while (int3 >= int4) {
+        int4++;
+    }
+    cout << "int3=" << int3 << "  " << "int4=" << int4 << endl;
+
+    int1 = 15;
+    int2 = 20;
+    cout << "int1=" << int1 << "  " << "int2=" << int2 << endl;
+    while (int1 <= int2) {
+        int1++;
+    }
+    cout << "int1=" << int1 << "  " << "int2=" << int2 << endl;
+}
